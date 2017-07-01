2a65053809073dfad1b78c342fd44431d385028b
Jasonzj <925645402@qq.com>
2017-07-01 16:35:05 +0800

    [task43] style.scss: optimize ClassName and SCSS

--- JS/task43/pixGallery.js
+++ JS/task43/pixGallery.js
@@ -2,7 +2,7 @@
  * @Author: Jason 
  * @Date: 2017-06-30 15:57:23 
  * @Last Modified by: Jason
- * @Last Modified time: 2017-06-30 18:58:25
+ * @Last Modified time: 2017-07-01 16:33:35
  */
 
 ;(function(root, factory) {
@@ -18,9 +18,19 @@
     'use strict'
 
     class pixGallery {
+        /**
+         * Creates an instance of pixGallery.
+         * @param {Object} JSON 
+         * {
+         *   container: '.pixGallway-1',    指定的容器
+         *   urls: [                        (可选)如果图片需要占位图loading，指定加载的图片
+         *       'http://placehold.it/2000x1000/E97452/fff'
+         *   ]
+         * }
+         * @memberof pixGallery
+         */
         constructor(JSON) {
             this.container = document.querySelector(JSON.container || '.pixGallway')
-            this.imgs = this.container.getElementsByTagName('img')
             this.height = this.container.clientHeight
             this.width = this.container.clientWidth
             this.urls = JSON.urls || false
@@ -28,14 +38,26 @@
             this.init()
         }
 
+        /**
+         * 初始化
+         * 
+         * @memberof pixGallery
+         */
         init() {
-            this.setSize()
             if (this.urls) {
                 this.setLoadingImage()
             }
+            this.setSize()
         }
 
+        /**
+         * 设置尺寸
+         * 
+         * @memberof pixGallery
+         */
         setSize() {
+            this.setImgs()
+            this.setClassN(this.container, `pixImgs${this.imgs.length}`)
             switch (this.imgs.length) {
                 case 3:
                     const sizeH = Math.ceil(this.height / 2)
@@ -55,6 +77,12 @@
             }
         }
 
+        /**
+         * 创建图片
+         * 
+         * @returns setSrc
+         * @memberof pixGallery
+         */
         createImage() {
             const imgNode = document.createElement('img')
             this.container.appendChild(imgNode)
@@ -62,25 +90,48 @@
             return {
                 setSrc(src) {
                     imgNode.src = src
-                },
-                setID(id) {
-                    imgNode.id = id
                 }
             }
         }
 
+        /**
+         * 设置加载图片
+         * 
+         * @memberof pixGallery
+         */
         setLoadingImage() {
             this.urls.forEach(item => {
                 const cImg = this.createImage(),
                     image = new Image()
 
                 cImg.setSrc('./loading.gif')
-                cImg.setID('loading')
                 image.src = item
                 image.onload = () => {
                     cImg.setSrc(item)
+                    this.setImgs()
+                    this.setClassN(this.container, `pixImgs${this.imgs.length}`)
                 }
             })
+        }
+
+        /**
+         * 设置this.imgs(所有图片Array)
+         * 
+         * @memberof pixGallery
+         */
+        setImgs() {
+            this.imgs = this.container.getElementsByTagName('img')
+        }
+
+        /**
+         * 添加目标className
+         * 
+         * @param {Element} target 目标元素
+         * @param {String} classN 指定的className
+         * @memberof pixGallery
+         */
+        setClassN(target, classN) {
+            target.classList.add(classN)
         }
     }
 
