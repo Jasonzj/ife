d725ef52ed76f74a6916804039cd55b479f3b032
Jasonzj <925645402@qq.com>
2017-07-16 20:56:05 +0800

    add task50

---
+++ JS/task50/src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const App = () => (
+    <div className="app">app</div>
+)
+
+export default App
---
+++ JS/task50/webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const webpack = require('webpack')
+
+module.exports = {
+    entry: ['./src/entry.js'],
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'js/[name].bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.scss', '.css'],
+        modules: [
+            path.resolve(__dirname, 'node_modules'),
+            path.join(__dirname, './src')
+        ],
+        alias: {
+            "action" : path.resolve(__dirname, 'src/action'),
+            "components" : path.resolve(__dirname, 'src/components'),
+            "containers" : path.resolve(__dirname, 'src/containers'),
+            "reducers" : path.resolve(__dirname, 'src/reducers'),
+            "utils" : path.resolve(__dirname, 'src/utils')
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                loader: 'babel-loader',
+                exclude: path.resolve(__dirname, 'node_modules'),
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            },
+            {
+                test: /\.(scss|css)$/,
+                exclude: path.resolve(__dirname, 'node_modules'),
+                use: ExtractTextPlugin.extract({
+                    use: ["css-loader", "sass-loader", "postcss-loader"]
+                })
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                exclude: path.resolve(__dirname, 'node_modules'),
+                use: ['url-loader?limit=1000&name=files/[md5:hash:base64:10].[ext]']
+            }
+        ]
+    },
+    devServer: {
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+        port: 8000,
+        contentBase: path.resolve(__dirname, "build"),
+        stats: {
+            modules: false,
+            chunks: false
+        }
+    },
+    plugins: [
+        new ExtractTextPlugin('styles.css'),
+        new htmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'index.html'
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ]
+}
