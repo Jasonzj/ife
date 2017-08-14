4fc10ef26005cc2ef6d4d04fe592447a5a981384
Jasonzj <925645402@qq.com>
2017-08-14 23:40:28 +0800

    [task25_ts]: add toggleTree

--- JS/task25_ts/js/tree.js
+++ JS/task25_ts/js/tree.js
@@ -1,26 +1,76 @@
 var JTree = (function () {
     function JTree(config) {
+        var _this = this;
         this.config = config;
+        this.clickEvent = function (e) {
+            var target = e.target;
+            var className = target.className;
+            if (className.includes(_this.config.iconClass)) {
+                // const icon = target.querySelector(`.${this.iconClassName}`)
+                var state = target.getAttribute('state');
+                var bool = state === 'true' ? false : true;
+                var parent_1 = target.parentNode.parentNode;
+                var children = Array.from(parent_1.children);
+                _this.toggleTree(children, target, bool);
+            }
+        };
         this.root = document.querySelector(this.config.root);
         this.data = this.config.data;
+        this.titleClassName = this.config.titleClass || 'tree_title';
+        this.wrapClassName = this.config.wrapClass || 'tree_wrap';
+        this.iconClassName = this.config.iconClass || 'tree_icon';
         this.init();
     }
     JTree.prototype.init = function () {
         this.initTree(this.data, this.root);
+        this.bindEvent();
     };
     JTree.prototype.initTree = function (data, dom) {
-        for (var name_1 in data) {
+        for (var titleName in data) {
             var wrap = document.createElement('div');
-            var title = document.createElement('div');
-            title.innerHTML = "" + name_1;
-            title.className = 'tree_title';
+            var title = document.createElement('h2');
+            var icon = document.createElement('span');
+            wrap.className = this.wrapClassName;
+            icon.className = this.iconClassName;
+            icon.setAttribute('state', 'false');
+            title.className = this.titleClassName;
+            title.innerHTML = "" + titleName;
+            if (JTree.isEmptyObject(data[titleName])) {
+                icon.classList.add('hide');
+            }
+            title.appendChild(icon);
             wrap.appendChild(title);
             dom.appendChild(wrap);
-            this.initTree(data[name_1], wrap);
+            this.initTree(data[titleName], wrap);
         }
+    };
+    JTree.prototype.toggleTree = function (children, icon, bool) {
+        var _this = this;
+        var name = bool ? 'add' : 'remove';
+        icon.setAttribute('state', "" + bool);
+        icon.classList[name]('show');
+        children.forEach(function (item) {
+            if (item.className !== _this.config.titleClass) {
+                item.classList[name]('show');
+            }
+        });
+    };
+    JTree.prototype.bindEvent = function () {
+        this.root.addEventListener('click', this.clickEvent);
     };
     return JTree;
 }());
+/**
+ * 判断空对象
+ * @static
+ * @return {Boolean} 是空对象返回ture，否则false
+ */
+JTree.isEmptyObject = function (obj) {
+    for (var key in obj) {
+        return false;
+    }
+    return true;
+};
 var data = {
     '目录': {
         'Javascript': {
@@ -50,6 +100,9 @@ var data = {
 };
 new JTree({
     root: '.box',
+    wrapClass: 'tree-body',
+    titleClass: 'tree-title',
+    iconClass: 'tree-icon',
     data: data
 });
 //# sourceMappingURL=tree.js.map
--- JS/task25_ts/ts/tree.ts
+++ JS/task25_ts/ts/tree.ts
@@ -1,36 +1,104 @@
 interface ITree{
     root: string,
-    data: any
+    data: any,
+    wrapClass?: string,
+    titleClass?: string,
+    iconClass?: string
+}
+
+interface ArrayConstructor {
+    from(arrayLike: any, mapFn?, thisArg?): Array<any>
 }
 
 class JTree {
     data: any
     root: Element
+    titleClassName: string
+    wrapClassName: string
+    iconClassName: string
     
     constructor(public config: ITree) {
         this.root = document.querySelector(this.config.root)
         this.data = this.config.data
+        this.titleClassName = this.config.titleClass || 'tree_title'
+        this.wrapClassName = this.config.wrapClass || 'tree_wrap'
+        this.iconClassName = this.config.iconClass || 'tree_icon'
 
         this.init()
     }
 
-    private init() {
+    init() {
         this.initTree(this.data, this.root)
+        this.bindEvent()
     }
 
-    private initTree(data: any, dom: Element) {
-        for (const name in data) {
-            const wrap = document.createElement('div')
-            const title = document.createElement('div')
-            title.innerHTML = `${name}`
-            title.className = 'tree_title'
-
+    initTree(data: any, dom: Element) {
+        for (const titleName in data) {
+            const wrap: Element = document.createElement('div')
+            const title: Element = document.createElement('h2')
+            const icon: Element = document.createElement('span')
+            wrap.className = this.wrapClassName
+            icon.className = this.iconClassName
+            icon.setAttribute('state', 'false')
+            title.className = this.titleClassName
+            title.innerHTML = `${titleName}`
+            
+            if (JTree.isEmptyObject(data[titleName])) {
+                icon.classList.add('hide')
+            }
+            
+            title.appendChild(icon)
             wrap.appendChild(title)
             dom.appendChild(wrap)
-            this.initTree(data[name], wrap)
+            this.initTree(data[titleName], wrap)
         }
     }
 
+    clickEvent = (e) => {
+        const target: any = e.target
+        const className: any = target.className
+
+        if (className.includes(this.config.iconClass)) {
+            const state: string = target.getAttribute('state')
+            const bool: boolean = state === 'true' ? false : true
+            const parent: Element = target.parentNode.parentNode
+            const children: Element[] = Array.from(parent.children)
+            
+            this.toggleTree(children, target, bool)
+        }
+    }
+
+    toggleTree(
+        children: Element[],
+        icon: Element,
+        bool: boolean
+    ) {
+        const name: string = bool ? 'add' : 'remove'
+        icon.setAttribute('state', `${bool}`)
+        icon.classList[name]('show')
+
+        children.forEach(item => {
+            if (item.className !== this.config.titleClass) {
+                item.classList[name]('show')
+            }
+        })
+    }
+
+    bindEvent() {
+        this.root.addEventListener('click', this.clickEvent)
+    }
+
+    /**
+     * 判断空对象
+     * @static
+     * @return {Boolean} 是空对象返回ture，否则false
+     */
+    static isEmptyObject = (obj) => {
+        for (const key in obj) {
+            return false
+        }
+        return true
+    }
 }
 
 const data = {
@@ -65,5 +133,8 @@ const data = {
 
 new JTree({
     root: '.box',
+    wrapClass: 'tree-body',
+    titleClass: 'tree-title',
+    iconClass: 'tree-icon',
     data
 })
