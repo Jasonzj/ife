75ac9df1ef1e2683f27ba21285893c55da52739f
Jasonzj <925645402@qq.com>
2017-08-14 20:18:40 +0800

    [task25_ts]: update tree.ts

--- JS/task25_ts/js/tree.js
+++ JS/task25_ts/js/tree.js
@@ -1,17 +1,55 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 var JTree = (function () {
     function JTree(config) {
         this.config = config;
         this.root = document.querySelector(this.config.root);
         this.data = this.config.data;
-        console.log('object');
         this.init();
     }
     JTree.prototype.init = function () {
-        console.log('object');
+        this.initTree(this.data, this.root);
+    };
+    JTree.prototype.initTree = function (data, dom) {
+        for (var name_1 in data) {
+            var wrap = document.createElement('div');
+            var title = document.createElement('div');
+            title.innerHTML = "" + name_1;
+            title.className = 'tree_title';
+            wrap.appendChild(title);
+            dom.appendChild(wrap);
+            this.initTree(data[name_1], wrap);
+        }
     };
     return JTree;
 }());
-exports.default = JTree;
+var data = {
+    '目录': {
+        'Javascript': {
+            '数组': {
+                '方法': {},
+                '技巧': {}
+            },
+            '对象': {
+                '工厂模式': {
+                    '缺点': {}
+                },
+                '原型模式': {},
+                '构造函数模式': {}
+            }
+        },
+        'CSS': {
+            '选择器': {
+                '各类': {}
+            },
+            '伪类': {
+                '没啥': {},
+                '没啥哦': {}
+            }
+        },
+        'HTML': {}
+    }
+};
+new JTree({
+    root: '.box',
+    data: data
+});
 //# sourceMappingURL=tree.js.map
--- JS/task25_ts/ts/tree.ts
+++ JS/task25_ts/ts/tree.ts
@@ -3,19 +3,67 @@ interface ITree{
     data: any
 }
 
-export default class JTree {
+class JTree {
     data: any
     root: Element
     
     constructor(public config: ITree) {
         this.root = document.querySelector(this.config.root)
         this.data = this.config.data
-        console.log('object');
+
         this.init()
     }
 
     private init() {
-        console.log('object')
+        this.initTree(this.data, this.root)
+    }
+
+    private initTree(data: any, dom: Element) {
+        for (const name in data) {
+            const wrap = document.createElement('div')
+            const title = document.createElement('div')
+            title.innerHTML = `${name}`
+            title.className = 'tree_title'
+
+            wrap.appendChild(title)
+            dom.appendChild(wrap)
+            this.initTree(data[name], wrap)
+        }
     }
 
 }
+
+const data = {
+    '目录': {
+        'Javascript': {
+            '数组': {
+                '方法': {},
+                '技巧': {}
+            },
+            '对象': {
+                '工厂模式': {
+                    '缺点': {}
+                },
+                '原型模式': {},
+                '构造函数模式': {}
+            }
+        },
+        'CSS' : {
+            '选择器': {
+                '各类': {}
+            },
+            '伪类': {
+                '没啥': {},
+                '没啥哦': {}
+            }
+        },
+        'HTML': {
+
+        }
+    }
+}
+
+new JTree({
+    root: '.box',
+    data
+})
