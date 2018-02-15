76f50fd4c47fd3b99491b357af3bb0c3fc4adae1
Jasonzj <925645402@qq.com>
2018-02-15 13:58:45 +0800

    [task21]: remove semi

--- JS/task21/task.js
+++ JS/task21/task.js
@@ -11,20 +11,20 @@
 (function() {
 
 	//私有属性
-	var Regular = /[^0-9a-zA-Z\u4e00-\u9fa5]+/;
-	var Regular2 = /[&\|\\\*^%'".+_=,;}{?><~`$#@\s/-]/ig;
-	var Regular3 = /[,\s\n]+/;
+	var Regular = /[^0-9a-zA-Z\u4e00-\u9fa5]+/
+	var Regular2 = /[&\|\\\*^%'".+_=,;}{?><~`$#@\s/-]/ig
+	var Regular3 = /[,\s\n]+/
 
 	//构造函数
 	function Queue (input, output, button) {
 		//公有属性
-		this.input = document.getElementById(input);
-		this.output = document.getElementById(output);
-		this.button = document.getElementById(button);
-		this.arr = [];
+		this.input = document.getElementById(input)
+		this.output = document.getElementById(output)
+		this.button = document.getElementById(button)
+		this.arr = []
 
 		//初始化
-		this.button ? this.init('buttonEvent') : this.init('keyEvent');
+		this.button ? this.init('buttonEvent') : this.init('keyEvent')
 	}
 
  	/**
@@ -40,8 +40,8 @@
  		*/
 
  		init: function (type) {
- 			var self = this;
-			self.setQueue(type);
+ 			var self = this
+			self.setQueue(type)
  		},
 
  		/**
@@ -51,8 +51,8 @@
  		*/
 
  		setArr: function (arr, num) {
- 			arr.splice(0, num);
- 			this.setRender(arr);
+ 			arr.splice(0, num)
+ 			this.setRender(arr)
  		},
 
  		/**
@@ -62,18 +62,18 @@
  		*/
 
  		setRender: function (arr) {
- 			var self = this;
- 			var num = 0;
- 			self.output.innerHTML = '';
+ 			var self = this
+ 			var num = 0
+ 			self.output.innerHTML = ''
  			for (var i = 0, l = arr.length; i < l; i++) {
 				if (l > 10) {
 					self.setArr(arr, l - 10)
 					return
 				} else {
- 					self.output.innerHTML += "<span class='smallbox' index="+ i +">"+ arr[i] +"</span>";
+ 					self.output.innerHTML += "<span class='smallbox' index="+ i +">"+ arr[i] +"</span>"
  				}
  			}
- 			num = 0;
+ 			num = 0
  		},
 
  		/**
@@ -85,18 +85,18 @@
 	 		if (e.target && e.target.nodeName === "SPAN") {
 		 		switch (e.type) {
 		 			case "mouseover":
-					 	e.target.textContent = '删除:' + e.target.textContent;
-		 				break;
+					 	e.target.textContent = '删除:' + e.target.textContent
+		 				break
 		 			case "mouseout":
-		 				e.target.textContent = e.target.textContent.replace('删除:','');
-		 				break;
+		 				e.target.textContent = e.target.textContent.replace('删除:','')
+		 				break
 		 			case "click":
-		 				var self = this;
-		 				self.output.removeChild(e.target);
-		 				var index = e.target.getAttribute('index');
-		 				self.arr.splice(index, 1);
-		 				self.setRender(self.arr);
-		 				break;
+		 				var self = this
+		 				self.output.removeChild(e.target)
+		 				var index = e.target.getAttribute('index')
+		 				self.arr.splice(index, 1)
+		 				self.setRender(self.arr)
+		 				break
 		 		}
 		 	}
  		},
@@ -106,13 +106,13 @@
  		*/
 
  		addTag: function (e) {
- 			var self = this;
+ 			var self = this
  			if (Regular3.test(e.target.value) || e.keyCode == 13) {		//判断按键逗号，回车和空格
- 				var str = e.target.value.replace(Regular2,'');
- 				self.arr.push(str);
- 				self.arr = self.arr.unique();
- 				e.target.value = '';
- 				self.setRender(self.arr);
+ 				var str = e.target.value.replace(Regular2,'')
+ 				self.arr.push(str)
+ 				self.arr = self.arr.unique()
+ 				e.target.value = ''
+ 				self.setRender(self.arr)
  			}
  		},
 
@@ -121,13 +121,13 @@
  		*/
 
  		addHobby: function () {
- 			var self = this;
- 			var str = self.input.value.trim();
- 			self.input.value = '';
- 			var data = str.split(Regular).filter( (e) => e.length !== 0 );
- 			Array.prototype.push.apply(self.arr, data);
- 			self.arr = self.arr.unique();
- 			self.setRender(self.arr);
+ 			var self = this
+ 			var str = self.input.value.trim()
+ 			self.input.value = ''
+ 			var data = str.split(Regular).filter( (e) => e.length !== 0 )
+ 			Array.prototype.push.apply(self.arr, data)
+ 			self.arr = self.arr.unique()
+ 			self.setRender(self.arr)
  		},
 
  		/**
@@ -136,15 +136,15 @@
  		*/
 
  		setQueue: function (type) {
- 			var self = this;
- 			addEvent(self.output, 'mouseover', self.setText);
- 			addEvent(self.output, 'mouseout', self.setText);
- 		 	addEvent(self.output, 'click', self.setText.bind(this));
+ 			var self = this
+ 			addEvent(self.output, 'mouseover', self.setText)
+ 			addEvent(self.output, 'mouseout', self.setText)
+ 		 	addEvent(self.output, 'click', self.setText.bind(this))
 
  		 	if (type === 'buttonEvent') {
- 		 		addEvent(self.button, 'click', self.addHobby.bind(this));
+ 		 		addEvent(self.button, 'click', self.addHobby.bind(this))
  		 	} else if (type === 'keyEvent') {
- 		 		addEvent(self.input, 'keyup', self.addTag.bind(this));
+ 		 		addEvent(self.input, 'keyup', self.addTag.bind(this))
  		 	}
  		}
 
@@ -156,30 +156,30 @@
 
  	//数组去重
 	Array.prototype.unique = function () {
- 		var res = [];
+ 		var res = []
  		this.forEach( (item, i) => res.indexOf(item) === -1 ? res.push(item) : '' )
- 		return res;
+ 		return res
  	}
 
  	//跨浏览器事件绑定
  	function addEvent (element, event, hanlder) {
  		if (element.addEventListener) {
- 			element.addEventListener(event, hanlder, false);
+ 			element.addEventListener(event, hanlder, false)
  		} else if (ele.attachEvent) {
- 			ele.attachEvent("on" + event, hanlder);
+ 			ele.attachEvent("on" + event, hanlder)
  		} else {
- 			element["on" + event] = hanlder;
+ 			element["on" + event] = hanlder
  		}
  	}
 
  	//外部接口
- 	window.Queue = Queue;
+ 	window.Queue = Queue
 
-})();
+})()
 
 //创建新实例
-new Queue("textArea", "output", "subLike");
-new Queue("textArea2", "output2", "subLike2");
-new Queue("textArea3", "output3", "subLike3");
-new Queue("tagInp", "tagBox");
-new Queue("tagInp2", "tagBox2");
+new Queue("textArea", "output", "subLike")
+new Queue("textArea2", "output2", "subLike2")
+new Queue("textArea3", "output3", "subLike3")
+new Queue("tagInp", "tagBox")
+new Queue("tagInp2", "tagBox2")
